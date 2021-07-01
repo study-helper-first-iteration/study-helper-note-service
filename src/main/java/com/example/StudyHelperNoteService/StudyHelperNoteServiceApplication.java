869756f493b9dc08a752bad702a8e26efd819ca9
Jason Benckert <jason.benckert@cognizant.com>
2021-07-01 16:24:26 -0400

    project initialization

---
+++ src/main/java/com/example/StudyHelperNoteService/StudyHelperNoteServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.StudyHelperNoteService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyHelperNoteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyHelperNoteServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/StudyHelperNoteService/StudyHelperNoteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StudyHelperNoteService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyHelperNoteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
